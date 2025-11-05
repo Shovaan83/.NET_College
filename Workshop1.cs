33b73f1e7f40fd4dc0f2e4e22dccb55205e06b80
Shovaan83 <shovanbthr@gmail.com>
2025-11-05 08:15:13 +0545

    Adding Excersice upto week 2

---
+++ Workshop1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp
+{
+	internal class Workshop1
+	{
+		public static void PrintQuote()
+		{
+			string quoteOfTheDay = "Success is not final, failure is not fatal. It is the courage to continue that counts.";
+
+			Console.WriteLine("Motivational Quote of the Day");
+			Console.WriteLine(quoteOfTheDay);
+		}
+	}
+}
