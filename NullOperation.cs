2772e67d43c6557c192f648bf0f61babf2ed8f28
Shovaan83 <shovanbthr@gmail.com>
2025-11-12 09:28:36 +0545

    Week 3 Task Completed

---
+++ NullOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class NullOperation
+    {
+        public static void PerformNullChecks()
+        {
+            string username = null;
+            string showUsername = username ?? "Username is not available";
+
+            Console.WriteLine($"Username: {showUsername}");
+        }
+
+        public static void PerformNullCoalescing()
+        {
+            string? usernameAnother = null;
+            usernameAnother ??= "Shovan";
+
+            Console.WriteLine($"Username: {usernameAnother}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,41 +6,68 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello My Name is Shovan");
-            Console.WriteLine("I live in Itahari, Sunsari");
-            Console.WriteLine("My phone number is 9825948059");
-            Console.WriteLine("College: Itahari Internation College");
+            //Console.WriteLine("Hello My Name is Shovan");
+            //Console.WriteLine("I live in Itahari, Sunsari");
+            //Console.WriteLine("My phone number is 9825948059");
+            //Console.WriteLine("College: Itahari Internation College");
 
             Console.WriteLine();
 
-            Workshop1.PrintQuote();
+            //Workshop1.PrintQuote();
 
-            Console.WriteLine("\nPrinting Task 1");
-            Task1.RunTask1();
+            //Console.WriteLine("\nPrinting Task 1");
+            //Task1.RunTask1();
 
-            Console.WriteLine("\nPriting Task 2");
+            //Console.WriteLine("\nPriting Task 2");
 
-            //Circle.PI = 3.1456;
+            ////Circle.PI = 3.1456;
 
-            Console.WriteLine($"\nValue of PI constant: {Circle.PI}");
+            //Console.WriteLine($"\nValue of PI constant: {Circle.PI}");
 
-            double radius = 3.0;
+            //double radius = 3.0;
 
-            Console.WriteLine($"\nThe area of the circle with radius of {radius} is : {Circle.CalculateArea(radius)}");
+            //Console.WriteLine($"\nThe area of the circle with radius of {radius} is : {Circle.CalculateArea(radius)}");
 
-            Console.WriteLine("\nPrinting Task 3");
+            //Console.WriteLine("\nPrinting Task 3");
 
-            Task3.RunTask3();
+            //Task3.RunTask3();
 
-            Console.WriteLine("\nPrinting Task 4");
-            Task4.RunTask4();
+            //Console.WriteLine("\nPrinting Task 4");
+            //Task4.RunTask4();
 
-            Console.WriteLine("\nPrinting Task 5");
-            Task5.RunTask5();
+            //Console.WriteLine("\nPrinting Task 5");
+            //Task5.RunTask5();
 
-            Console.WriteLine("\nPrinting Task 6");
-            Task6.RunTask6();
+            //Console.WriteLine("\nPrinting Task 6");
+            //Task6.RunTask6();
 
+            Operators.Multiply(2, 3);
+            Operators.Add(5, 7);
+            Operators.Subtract(10, 4);
+            Operators.Divide(20, 5);
+            Operators.OddEvenFinder(9);
+
+            Console.WriteLine();
+            NullOperation.PerformNullChecks();
+            NullOperation.PerformNullCoalescing();
+
+            Console.WriteLine();
+            IfElseSwitch.Age();
+
+            Console.WriteLine();
+            Week.WeekDay();
+
+            Console.WriteLine();
+            Loops.SumN();
+            Console.WriteLine();
+            Loops.WhileCheck();
+            Console.WriteLine();
+            Loops.ForEach();
+
+            Console.WriteLine();
+            Exceptions.InvalidNum();
+            Console.WriteLine();
+            Exceptions.PasswordCheck();
             Console.ReadLine();
         }
 
